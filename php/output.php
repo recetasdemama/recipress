9825dd88ee3a9c298318f3b2b7670aec51d172ba
Francisco de Juan <francisco.juan@gmail.com>
2014-01-29 13:16:20 +0100

    Change on the output file required for the new theme

--- php/output.php
+++ php/output.php
@@ -6,28 +6,13 @@ function get_the_recipe() {
 	// determine if post has a recipe
 	if(has_recipress_recipe() && recipress_output()) {
 		// create the array
-		$recipe['before'] = '<div class="hrecipe '.recipress_theme().'" id="recipress_recipe">';
-		$recipe['title'] = '<h2 class="fn">'.recipress_recipe('title').'</h2>';
+		$recipe['before'] = '<div class=" '.recipress_theme().'" id="recipress_recipe">';
+		$recipe['title'] = '';
 		$recipe['photo'] = recipress_recipe('photo', 'class=photo recipress_thumb');
 		$recipe['meta'] = '<p class="seo_only">'.__('By', 'recipress').' <span class="author">'.get_the_author().'</span>
 							'.__('Published:', 'recipress').' <span class="published updated">'.get_the_date('F j, Y').'<span class="value-title" title="'.get_the_date('c').'"></span></span></p>';
 							
-		// details
-		$recipe['details_before'] = '<ul class="recipe-details">';
-		if(recipress_recipe('yield'))
-			$recipe['yield'] = '<li id="yield"><b>'.__('Yield:', 'recipress').'</b> <span class="yield">'.recipress_recipe('yield').'</span></li>';
-		if(recipress_recipe('cost'))
-			$recipe['cost'] = '<li><b>'.__('Cost:', 'recipress').'</b> <span class="cost">'.recipress_recipe('cost').'</span></li>';
-		if(recipress_recipe('prep_time') && recipress_recipe('cook_time'))
-			$recipe['clear_items'] = "</ul><ul class='recipe-details right'>";
-			/*$recipe['clear_items'] = '<li class="clear_items"></li>';*/
-		if(recipress_recipe('prep_time'))
-			$recipe['prep_time'] = '<li id="prep"><b>'.__('Prep:', 'recipress').'</b> <span class="preptime"><span class="value-title" title="'.recipress_recipe('prep_time', 'iso').'"></span>'.recipress_recipe('prep_time','mins').'</span></li>';
-		if(recipress_recipe('cook_time'))
-			$recipe['cook_time'] = '<li><b>'.__('Cook:', 'recipress').'</b> <span class="cooktime"><span class="value-title" title="'.recipress_recipe('cook_time','iso').'"></span>'.recipress_recipe('cook_time','mins').'</span></li>';
-		if(recipress_recipe('prep_time') && recipress_recipe('cook_time'))
-			$recipe['ready_time'] ='<li><b>'.__('Ready In:', 'recipress').'</b> <span class="duration"><span class="value-title" title="'.recipress_recipe('ready_time','iso').'"></span>'.recipress_recipe('ready_time','mins').'</span></li>';
-		$recipe['details_after'] = '</ul>';
+		
 		
 		// summary
 		$summary = recipress_recipe('summary');
@@ -35,14 +20,44 @@ function get_the_recipe() {
 			$recipe['summary'] = '<p class="summary seo_only">'.recipress_gen_summary().'</p>';
 		else
 			$recipe['summary'] = '<p class="summary">'.$summary.'</p>';
+		// instructions
+		$recipe['instructions_title'] = '<div class="Cinstructions"><h3>'.__('Instructions', 'recipress').'</h3>';
+		$recipe['instructions'] = recipress_instructions_list().'</div>';
+		
+		
+		$recipe['global_ingredients_before'] = '<div class="SidebarSingleingredients"><div class="Cingredients"><div class="clipSolo"></div>';
+		// details
+		$recipe['details_before'] = '<ul class="recipe-details det-sup">';
+		if(recipress_recipe('yield'))
+			$recipe['yield'] = '<li id="yield" class="raci"><span class="yield">'.recipress_recipe('yield').'</span></li>';
+		/*if(recipress_recipe('cost'))
+			$recipe['cost'] = '<li><b>'.__('Cost:', 'recipress').'</b> <span class="cost">'.recipress_recipe('cost').'</span></li>';*/
+		if(recipress_recipe('prep_time') && recipress_recipe('cook_time'))
+			$recipe['ready_time'] ='<li class="tTotal">'.__('Ready In:', 'recipress').' <span class="duration"><span class="value-title" title="'.recipress_recipe('ready_time','iso').'"></span>'.recipress_recipe('ready_time','mins').'</span></li>';
+		
+		if(recipress_recipe('prep_time') && recipress_recipe('cook_time'))
+			$recipe['clear_items'] = "</ul><ul class='recipe-details detalles-abajo'><div class='flechaArriba'></div>";
+			/*$recipe['clear_items'] = '<li class="clear_items"></li>';*/
+		if(recipress_recipe('prep_time'))
+			$recipe['prep_time'] = '<li class="prep" id="prep">'.__('Prep:', 'recipress').' <span class="preptime"><span class="value-title" title="'.recipress_recipe('prep_time', 'iso').'"></span>'.recipress_recipe('prep_time','mins').' +</span></li>';
+		if(recipress_recipe('cook_time'))
+			$recipe['cook_time'] = '<li class="cook">'.__('Cook:', 'recipress').' <span class="cooktime"><span class="value-title" title="'.recipress_recipe('cook_time','iso').'"></span>'.recipress_recipe('cook_time','mins').'</span></li>';
+		
+		$recipe['details_after'] = '</ul>';
 		
 		// indredients
 		$recipe['ingredients_title'] = '<h3>'.__('Ingredients', 'recipress').'</h3>';
 		$recipe['ingredients'] = recipress_ingredients_list();
+		$recipe['global_ingredients_after1'] = '</div>';
+		if ( is_active_sidebar( 'sidebar-single' ) ) : 
+			ob_start();
+			dynamic_sidebar('sidebar-single');
+			$sidebar = ob_get_contents();
+			ob_end_clean();
+			$recipe['sidebar_single'] = '<div id="sidebar-coments" class="widget-area" role="complementary">'.$sidebar.'</div>';
+    	endif;
+		$recipe['global_ingredients_after2'] = '</div>';
 		
-		// instructions
-		$recipe['instructions_title'] = '<h3>'.__('Instructions', 'recipress').'</h3>';
-		$recipe['instructions'] = recipress_instructions_list();
     
 		// taxonomies
 		if(recipress_recipe('cuisine') || recipress_recipe('course') || recipress_recipe('skill_level') ) {
